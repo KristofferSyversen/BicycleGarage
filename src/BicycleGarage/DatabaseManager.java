9c95cd7c029c0d3d7db6434e8746ee1186cb3a23
EmilFriberg1 <emil.c.friberg@gmail.com>
2015-05-22 15:37:06 +0200

    Updates in Database manager

--- src/BicycleGarage/DatabaseManager.java
+++ src/BicycleGarage/DatabaseManager.java
@@ -1,5 +1,32 @@
 package BicycleGarage;
 
 public class DatabaseManager {
+	private Database database;
 
+	public DatabaseManager(Database database) {
+		this.database = database;
+	}
+	
+	public void addUser(String n, int i, String b){
+		if(database.getUser(b).equals(null)){
+			User user = new User(n, i, b);
+			database.addUser(user);
+		} else {
+			//felmeddelande
+		}
+	}
+	
+	public void removeUser(String b){
+		User user = database.getUser(b);
+		if(/*user has no bikes*/){
+			database.removeUser(user);
+		} else {
+			//felmeddelande
+		}
+	}
+	
+	public void addBicycle(User o, String b){
+		Bicycle bicycle = new Bicycle(o, b);
+		if()
+	}
 }
