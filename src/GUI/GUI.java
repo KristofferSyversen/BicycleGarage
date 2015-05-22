08d778f79954d248f768362556871911c16c1d6c
KristofferSyversen <kristoffer.syversen@gmail.com>
2015-05-22 22:30:50 +0200

    Added a basic window structure.

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,7 +1,32 @@
 package GUI;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+
+import BicycleGarage.BicycleGarageManager;
+import BicycleGarage.DatabaseManager;
+import BicycleGarage.User;
+
 public class GUI {
-	public GUI(){
+	private DatabaseManager ddm;
+	private BicycleGarageManager bgm;
+	
+	JList<String> itemList;
+
+	public GUI(DatabaseManager ddm, BicycleGarageManager bgm) {
+		this.ddm = ddm;
+		this.bgm = bgm;
+
+		JFrame mainFrame = new JFrame("Bicycle garage operator controll system.");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		
+		itemList = new JList<String>();
+		mainFrame.add(itemList, BorderLayout.NORTH);
+
+		mainFrame.pack();
+		mainFrame.setVisible(true);
 	}
 }
